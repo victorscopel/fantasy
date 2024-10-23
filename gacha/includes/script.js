db79ce04ef3fe7ea8ca5a616f7b4d48f1eeb865b
Victor Scopel <victorscopel@hotmail.com>
2024-10-23 14:12:15 -0400

    update2

--- gacha/includes/script.js
+++ gacha/includes/script.js
@@ -207,13 +207,25 @@ $(document).ready(function() {
     // Abrir o popup e iniciar o vídeo
     $('.botaopet button').on('click', function() {
         $('#videoPopup').fadeIn();
+        $('#popupOverlay').fadeIn();
         $('#videoPlayer')[0].play();
     });
 
     // Fechar o popup e pausar o vídeo
     $('#closePopupButton').on('click', function() {
+        fecharPopup();
+    });
+
+    // Fechar o popup ao clicar na sobreposição
+    $('#popupOverlay').on('click', function() {
+        fecharPopup();
+    });
+
+    // Função para fechar o popup
+    function fecharPopup() {
         $('#videoPopup').fadeOut();
+        $('#popupOverlay').fadeOut();
         $('#videoPlayer')[0].pause();
         $('#videoPlayer')[0].currentTime = 0;
-    });
+    }
 });
